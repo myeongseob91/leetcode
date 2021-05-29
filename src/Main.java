dfca5b75d451bde627cd0c04bb7c02aebac0073d
심명섭 <simmyeongseob@simmyeongseob-ui-MacBookPro.local>
2021-05-29 15:02:43 +0900

    start

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("start");
+
+        MyLinkedList list = new MyLinkedList();
+        list.addAtHead(3);
+        list.addAtIndex(1,2);
+        list.addAtIndex(2,0);
+        list.addAtIndex(3,-4);
+
+        Solution sol = new Solution();
+
+
     }
 }
---
+++ src/Solution.java
@@ -0,0 +1,5 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+
+    }
+}
